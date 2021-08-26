e91a02539b7e11491472113bb7f8d292dc2bcb28
Oscar A. Gonzalez Alcerro <oscar.alcerro@galco.hn>
2021-08-26 13:59:39 -0600

    Initial commit

---
+++ src/com/company/test/Main.java
@@ -0,0 +1,22 @@
+package com.company.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //This will print Hello World to the console
+	System.out.println("Hello World");
+
+	//This will print Lets test this
+	System.out.println("Lets test this");
+
+	String name = "Oscar Andres";
+	System.out.println(name);
+
+	int myAge = 29;
+	System.out.println(myAge);
+
+	/*This is further test of comments
+	 This type of comment is used to write multiple lines of momments for longer explanaitions
+	like this one*/
+    }
+}
